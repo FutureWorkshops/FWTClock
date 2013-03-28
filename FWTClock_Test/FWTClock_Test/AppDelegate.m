c1dd8f5ef74991a7704c583914edf88f1ad505ef
Marco Meschini <marco@futureworkshops.com>
2013-03-28 18:03:41 +0000

    Update to latest Ristretto UI

--- FWTClock_Test/FWTClock_Test/AppDelegate.m
+++ FWTClock_Test/FWTClock_Test/AppDelegate.m
@@ -22,31 +22,15 @@
     self.window.backgroundColor = [UIColor whiteColor];
 
     //
-    RistrettoTableViewController *rootViewController = [[[RistrettoTableViewController alloc] init] autorelease];
     NSArray *samples = @[[RistrettoSampleDescriptor descriptorWithTitle:@"Default" className:@"DefaultViewController"],
                          [RistrettoSampleDescriptor descriptorWithTitle:@"Night" className:@"CustomViewController"],
                          ];
+
+    RistrettoTableViewController *rootViewController = [[[RistrettoTableViewController alloc] init] autorelease];
     rootViewController.items = samples;
-    rootViewController.title = @"FWTClock";
     
-    //
-    UIView *headerView = [[[UIView alloc] initWithFrame:(CGRect){.0f, .0f, 240.0f, 200.0f}] autorelease];
-    [rootViewController setTableHeaderView:headerView insets:(UIEdgeInsets){58.0f, 40.0f, 45.0f, 40.0f}];
-    
-    UIImageView *iconView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon_clock.png"]] autorelease];
-    iconView.center = CGPointMake(CGRectGetMidX(headerView.bounds), iconView.center.y);
-    [headerView addSubview:iconView];
-    
-    UILabel *label = [[[UILabel alloc] init] autorelease];
-    label.text = @"Choose a sample:";
-    label.font = [UIFont Ristretto_lightFontOfSize:18.0f];
-    label.textColor = [UIColor Ristretto_darkGrayColor];
-    [label sizeToFit];
-    label.center = CGPointMake(CGRectGetMidX(headerView.bounds), CGRectGetHeight(headerView.bounds)-CGRectGetHeight(label.bounds)*.5f);
-    [headerView addSubview:label];
-    
-    self.window.rootViewController = [UINavigationController Ristretto_navigationControllerWithRootViewController:rootViewController];
-        
+    self.window.rootViewController = [UINavigationController Ristretto_navigationControllerWithRootViewController:rootViewController
+                                                                                             defaultHeaderEnabled:YES];
     [self.window makeKeyAndVisible];
     return YES;
 }
